4d7050fa7e74ed64e86ce6e9189d9c8fe7146eec
Seth <sethdholton@utexas.edu>
2022-11-10 12:20:47 -0600

    rewrite of original draft
    rearranges inputs and outputs to match new prototype for the circuitry

--- once_upon_an_arduino_ish.ino
+++ once_upon_an_arduino_ish.ino
@@ -1,59 +1,88 @@
-const
-int switchPin1 = 8; //the switch connect to pin 12
-int switchPin2 = 9;
-int switchPin3 = 10;
+#include <Servo.h>
+int switchPin1 = 13;
+int switchPin2 = 12;
+int switchPin3 = 11;
+int switchPin4 = 10;
+int switchPin5 = 9;
+int switchPin6 = 8;
 
-const
-int ledPin = 13;//the led connect to pin 6
+int ledPin = 2;
+
+Servo servo1;
+
+bool switchesOn = false;
+bool firstStage = false;
+bool secondStage = false;
 
 /**********************************/
 
-int
-switchState1 = 0;
+int switchState1 = 0;
 int switchState2 = 0;
 int switchState3 = 0;
+int switchState4 = 0;
+int switchState5 = 0;
+int switchState6 = 0;
 
-void
-setup()
+int pos = 0;
+
+unsigned long previousMillis = 0;
+
+void setup()
 
 {
-
   pinMode(switchPin1, INPUT);
   pinMode(switchPin2, INPUT);
   pinMode(switchPin3, INPUT);
+  pinMode(switchPin4, INPUT);
+  pinMode(switchPin5, INPUT);
+  pinMode(switchPin6, INPUT);
 
-  pinMode(ledPin, OUTPUT); //initialize the led
-
+  pinMode(ledPin, OUTPUT);
+  servo1.attach(7);
+  servo1.write(0);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600);
 }
 
-/**********************************/
-
-void
-loop()
-
+void loop()
 {
-
-  //read the state of the switch value
-
+  unsigned long currentMillis = millis();
+  
   switchState1 = digitalRead(switchPin1);
   switchState2 = digitalRead(switchPin2);
   switchState3 = digitalRead(switchPin3);
-
-  if (switchState1 == HIGH) //if it is,the
-
+  switchState4 = digitalRead(switchPin4);
+  switchState5 = digitalRead(switchPin5);
+  switchState6 = digitalRead(switchPin6);
+  
+  servo1.write(pos);
+  if (switchState1 == HIGH && switchState2 == HIGH && 
+      switchState3 == HIGH && switchState4 == HIGH && 
+      switchState5 == HIGH && switchState6 == HIGH)
   {
-
-   
-digitalWrite(ledPin, HIGH); //turn the led on
-
+	digitalWrite(ledPin, HIGH);
+	switchesOn = true;
   }
-
   else
-
   {
-
-    digitalWrite(ledPin, LOW); //turn the led
-
+    digitalWrite(ledPin, LOW);
+    switchesOn = false;
+    firstStage = false;
+    secondStage = false;
+    pos = 0;
+    
+  }
+  
+  if (switchesOn && !firstStage){
+  	pos = 90;
+    firstStage = true;
+    previousMillis = currentMillis;
+  }
+  
+  if (firstStage && !secondStage && currentMillis - previousMillis == 1000){
+    pos = 180;
+    secondStage = true;
+    
   }
 
 }
