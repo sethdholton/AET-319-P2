1a8899905aad0b81fb53b2aee8e863489444522a
Seth <sethdholton@utexas.edu>
2022-11-13 19:20:56 -0600

    draft 2
    variables renamed to match switch function, servos work successfully. Implemented bird servo.

--- once_upon_an_arduino_ish.ino
+++ once_upon_an_arduino_ish.ino
@@ -1,45 +1,47 @@
 #include <Servo.h>
-int switchPin1 = 13;
-int switchPin2 = 12;
-int switchPin3 = 11;
-int switchPin4 = 10;
-int switchPin5 = 9;
-int switchPin6 = 8;
+/*Workstation Switch Variables*/
+int goodKitchSwitch = 13;
+int badKitchSwitch = 12;
+int badForestSwitch = 11;
+int goodForestSwitch = 10;
+int badWellSwitch = 9;
+int goodWellSwitch = 8;
 
-int ledPin = 2;
-
-Servo servo1;
+int goodKitchState = 0;
+int badKitchState = 0;
+int badForestState = 0;
+int goodForestState = 0;
+int badWellState = 0;
+int goodWellState = 0;
 
 bool switchesOn = false;
+
+/*Testing LED*/
+int ledPin = 2;
+
+/*Bird Talking Variables*/
 bool firstStage = false;
 bool secondStage = false;
 
-/**********************************/
+Servo birdServo;
+int birdPos = 0;
 
-int switchState1 = 0;
-int switchState2 = 0;
-int switchState3 = 0;
-int switchState4 = 0;
-int switchState5 = 0;
-int switchState6 = 0;
-
-int pos = 0;
-
+/*Timer Variables*/
 unsigned long previousMillis = 0;
 
 void setup()
 
 {
-  pinMode(switchPin1, INPUT);
-  pinMode(switchPin2, INPUT);
-  pinMode(switchPin3, INPUT);
-  pinMode(switchPin4, INPUT);
-  pinMode(switchPin5, INPUT);
-  pinMode(switchPin6, INPUT);
+  pinMode(goodKitchSwitch, INPUT);
+  pinMode(badKitchSwitch, INPUT);
+  pinMode(badForestSwitch, INPUT);
+  pinMode(goodForestSwitch, INPUT);
+  pinMode(badWellSwitch, INPUT);
+  pinMode(goodWellSwitch, INPUT);
 
   pinMode(ledPin, OUTPUT);
-  servo1.attach(7);
-  servo1.write(0);
+  birdServo.attach(6);
+  birdServo.write(0);
   digitalWrite(ledPin, LOW);
   Serial.begin(9600);
 }
@@ -48,39 +50,50 @@ void loop()
 {
   unsigned long currentMillis = millis();
   
-  switchState1 = digitalRead(switchPin1);
-  switchState2 = digitalRead(switchPin2);
-  switchState3 = digitalRead(switchPin3);
-  switchState4 = digitalRead(switchPin4);
-  switchState5 = digitalRead(switchPin5);
-  switchState6 = digitalRead(switchPin6);
+  goodKitchState = digitalRead(goodKitchSwitch);
+  badKitchState = digitalRead(badKitchSwitch);
+  badForestState = digitalRead(badForestSwitch);
+  goodForestState = digitalRead(goodForestSwitch);
+  badWellState = digitalRead(badWellSwitch);
+  goodWellState = digitalRead(goodWellSwitch);
   
-  servo1.write(pos);
-  if (switchState1 == HIGH && switchState2 == HIGH && 
-      switchState3 == HIGH && switchState4 == HIGH && 
-      switchState5 == HIGH && switchState6 == HIGH)
+  birdServo.write(birdPos);
+
+  if (goodKitchState == HIGH && goodForestState == HIGH && 
+      goodWellState == HIGH)
   {
-	digitalWrite(ledPin, HIGH);
-	switchesOn = true;
+    digitalWrite(ledPin, HIGH);
   }
   else
   {
     digitalWrite(ledPin, LOW);
+  }
+  
+  if (goodKitchState == HIGH && goodForestState == HIGH && 
+      goodWellState == HIGH)
+  {
+  //digitalWrite(ledPin, HIGH);
+  switchesOn = true;
+  }
+  else
+  {
+    //digitalWrite(ledPin, LOW);
     switchesOn = false;
     firstStage = false;
     secondStage = false;
-    pos = 0;
+    birdPos = 0;
     
   }
   
   if (switchesOn && !firstStage){
-  	pos = 90;
+    birdPos = 90;
     firstStage = true;
     previousMillis = currentMillis;
   }
+
   
-  if (firstStage && !secondStage && currentMillis - previousMillis == 1000){
-    pos = 180;
+  if (firstStage && !secondStage && currentMillis - previousMillis == 3000){
+    birdPos = 180;
     secondStage = true;
     
   }
