000d30345e113aa1017e18dbad7a4e4abdcfcce0
Seth <sethdholton@utexas.edu>
2022-11-16 10:12:51 -0600

    minor improvements and organization
    Added booleans and serial.println's for easier debugging and making sure everything's working while users are playtesting

--- once_upon_an_arduino_ish.ino
+++ once_upon_an_arduino_ish.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-/*Workstation Switch Variables*/
+//Workstation Switch Variables
 int goodKitchSwitch = 13;
 int badKitchSwitch = 12;
 int badForestSwitch = 11;
@@ -16,32 +16,38 @@ int goodWellState = 0;
 
 bool allDoingJobs = false;
 
-/*Testing LED*/
+//Testing LED
 int ledPin = 2;
 
-/*Bird Talking Variables*/
-
+//Bird Talking Variables
 Servo birdServo;
-int birdPos = 0;
+int birdPos = 180;
+
 bool speech1 = true;
 bool speech2 = false;
+bool birdLeave = false;
 
 //death variables
 Servo wellServo;
 int wellPos = 0;
+bool birdDeath = false;
 
 Servo dogServo;
 int dogPos = 0;
+bool sausDeath = false;
 
 Servo fireServo;
 int firePos = 0;
+bool mouseDeath = false;
 
-/*Timer Variables*/
+//Timer Variables
 unsigned long previousMillis = 0;
 
 //Story Variables
 bool act1 = true;
 bool act2 = false;
+bool scene1 = true;
+bool scene3 = true;
 void setup()
 
 {
@@ -54,7 +60,7 @@ void setup()
 
   pinMode(ledPin, OUTPUT);
   birdServo.attach(7);
-  birdServo.write(0);
+  birdServo.write(180);
 
   wellServo.attach(6);
   wellServo.write(0);
@@ -82,21 +88,17 @@ void loop()
 
   if (act1 && !act2)
   {
-  	Serial.println("act1");
-  if (goodKitchState == HIGH && goodForestState == HIGH && 
-      goodWellState == HIGH)
-  {
-    digitalWrite(ledPin, HIGH);
-  }
-  else
-  {
-    digitalWrite(ledPin, LOW);
-  }
-  
+    //Scene 1: Mouse, Bird, Sausage all do their jobs. 
+    if (scene1){
+      Serial.println("scene1");
+      scene1 = false;
+    }
+    
     if (goodKitchState == HIGH && goodForestState == HIGH && 
         goodWellState == HIGH){
       digitalWrite(ledPin, HIGH);
       allDoingJobs = true;
+      }
     }
     else{
       digitalWrite(ledPin, LOW);
@@ -104,32 +106,53 @@ void loop()
     }
     
     if (allDoingJobs && speech1 && !speech2){
+      //Scene 2: Other Bird talks to Bird and tells him that Sausage should get the wood instead.
+      Serial.println("scene2");
       birdServo.write(90);
       act1 = false;
       act2 = true;
     }
-  }
-  
   if (act2 && !act1){
-    Serial.println("act2");
-    if (badForestState == HIGH){
+    if (badForestState == HIGH && sausDeath == false){
+      if (scene3){
+      //Scene 3: While getting wood, Sausage gets eaten by a dog.
+      Serial.println("scene3");
+      scene3 = false;
+      }
       dogServo.write(180);
+      sausDeath = true;
       if (speech1 && !speech2){
         previousMillis = currentMillis;
         speech1 = false;
         speech2 = true;
       }
-      Serial.println(currentMillis - previousMillis);
       if (currentMillis - previousMillis >= 2000 && speech2){
-        birdServo.write(180);
+        //Scene 4: Other bird tells bird that Mouse should cook.
+        Serial.println("scene4");
+        birdServo.write(0);
+        speech2 = false;
       }
     }
-    if (badKitchState == HIGH){
+    if (badKitchState == HIGH && !mouseDeath){
+      //Scene 5: Mouse dies by cooking herself.
+      Serial.println("scene5");
       fireServo.write(180);
-      birdServo.write(0);
+      mouseDeath = true;
+      previousMillis = currentMillis;
+      birdLeave = true;
     }
-    if (badWellState == HIGH){
+    if (currentMillis - previousMillis >= 2000 && !speech1 && !speech2 && birdLeave){
+        birdServo.write(180);
+        birdLeave = false;
+      }
+    if (badWellState == HIGH && !birdDeath){
+      //Scene 6: Bird tries to extinguish house by getting water from the well, falls in.
+      Serial.println("scene6");
       wellServo.write(180);
+      birdDeath = true;
+    }
+    if (sausDeath == true && mouseDeath == true && birdDeath == true){
+      act2 = false;
     }
   }
 }
